e0515ef2ae66d145227f603c08c372cae779cfb8
clockdomain <anthony.reese.rocha@gmail.com>
2023-08-28 13:37:13 -0700

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,68 @@
+/// A simple job queue that can be used to queue up jobs to be 
+/// executed by the runtime.
+/// # Examples
+/// ```
+/// use caliptra_runtime::JobQueue;
+/// let mut job_queue = JobQueue::new();
+/// job_queue.add(|| {
+///    println!("Hello from a job!");
+///   Ok(())
+/// });
+/// job_queue.execute_next();
+/// ```
+/// # Notes
+/// 
+pub struct JobQueue {
+    /// The queue of jobs
+    queue: [Option<fn() -> CaliptraResult<()>>; 8],
+    /// The index of the next job to execute
+    next: usize,
+    /// The index of the next job to add
+    add: usize,
+}
+
+impl JobQueue {
+    /// Create a new instance of the job queue
+    pub fn new() -> Self {
+        Self {
+            queue: [None; 8],
+            next: 0,
+            add: 0,
+        }
+    }
+    /// Increment next index, wrap around if needed
+    fn increment_next(&mut self) {
+        self.next = (self.next + 1) % self.queue.len();
+    }
+    /// Increment add index, wrap around if needed
+    fn increment_add(&mut self) {
+        self.add = (self.add + 1) % self.queue.len();
+    }
+
+    /// Add a job to the queue
+    pub fn add(&mut self, job: fn() -> CaliptraResult<()>) -> CaliptraResult<()> {
+        // Check if the queue is full, return CaliptraError if it is
+        if self.queue[self.add].is_some() {
+            cprintln!("[rt] Job queue is full");
+            return Err(CaliptraError::RUNTIME_JOB_QUEUE_FULL);
+        }
+
+        self.queue[self.add] = Some(job);
+        self.increment_add();
+        Ok(())
+    }
+
+    /// Execute the next job in the queue
+    pub fn execute_next(&mut self) -> CaliptraResult<()> {
+        if let Some(job) = self.queue[self.next] {
+            job()?;
+            self.increment_next();
+        }
+        Ok(())
+    }
+}
+
+
 struct MemoryRegions {
     rom: &'static mut [u8],
     mbox: &'static mut [u8],
